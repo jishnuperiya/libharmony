2994902f1700f22879446c1f897789fb81d2f4ac
jishnuperiya <jishnuperiya@gmail.com>
2026-04-07 16:12:22 +0200

    magic number 12 -> constexpr var

--- src/scale_query.hpp
+++ src/scale_query.hpp
@@ -126,7 +126,7 @@ namespace harmony
     {
       p(s);
     }*/ 
-    inline std::vector<scale_entry> find_all(const predicate& pred)
+    [[nodiscard]]inline std::vector<scale_entry> find_all(const predicate& pred)
     {
       std::vector<scale_entry> result;
 
